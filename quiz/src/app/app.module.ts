28468a0658a9fee8b6ffdd29b5b94073c393d0f1
Alexander Masud <alexandermasud@protonmail.com>
2018-02-19 12:52:37 +0100

    Skapade komponenter

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ScoreBoardComponent } from './score-board/score-board.component';
+import { AboutComponent } from './about/about.component';
 
+
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'quiz', component: QuizComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    QuizComponent,
+    ScoreBoardComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
