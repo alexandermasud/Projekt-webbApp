a7f4255d1a8fe0f2c85b48e40b1628a8a624703b
Alex <alexandermasud@protonmail.com>
2018-02-19 17:23:18 +0100

    Viktig uppdatering
    Html filerna synkade inte som du skulle

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -1,8 +1,10 @@
+// Angular moduler
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
+// Applikationens komponenter
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,12 +14,14 @@ import { ScoreBoardComponent } from './score-board/score-board.component';
 import { ResultComponent } from './result/result.component';
 import { AboutComponent } from './about/about.component';
 
+// Applikationens API service
 import { HttpService } from '../app/quiz/quiz.service';
 
+// Routes för applikationen
 const appRoutes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'quiz', component: QuizComponent },
-  { path: 'result', component: ResultComponent},
+  //{ path: 'result', component: ResultComponent},
   { path: 'about', component: AboutComponent},
 ];
 
