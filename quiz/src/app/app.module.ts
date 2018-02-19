6bf2208fe58d65df57e0b4da142883912254fb10
Alexander Masud <alexandermasud@protonmail.com>
2018-02-19 12:53:45 +0100

    Skapande komponenter 2

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ScoreBoardComponent } from './score-board/score-board.component';
+import { ResultComponent } from './result/result.component';
 import { AboutComponent } from './about/about.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'quiz', component: QuizComponent },
+  { path: 'result', component: ResultComponent},
+  { path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
@@ -24,6 +27,7 @@ const appRoutes: Routes = [
     LandingComponent,
     QuizComponent,
     ScoreBoardComponent,
+    ResultComponent,
     AboutComponent
   ],
   imports: [
