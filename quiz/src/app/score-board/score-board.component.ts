6bb2fd59e09005d4b5e648be3eacf63bfc471ccd
Alexander Masud <alexandermasud@protonmail.com>
2018-02-26 11:01:32 +0100

    Påbörjade scoreboard

--- quiz/src/app/score-board/score-board.component.ts
+++ quiz/src/app/score-board/score-board.component.ts
@@ -9,7 +9,16 @@ export class ScoreBoardComponent implements OnInit {
 
   constructor() { }
 
+
+  scores: any[0];
+
+
+
   ngOnInit() {
+
+    this.scores = [{name: 'Alexander', score: 5739},{name: 'Viktor', score: 2}]
   }
+
+
 
 }
