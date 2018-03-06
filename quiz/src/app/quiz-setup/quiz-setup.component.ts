21672848f76c8a2575447e3fffdcee5ce581aecd
Alexander Masud <alexandermasud@protonmail.com>
2018-03-06 14:35:35 +0100

    Fixade breadcrumbs

--- quiz/src/app/quiz-setup/quiz-setup.component.ts
+++ quiz/src/app/quiz-setup/quiz-setup.component.ts
@@ -10,7 +10,7 @@ export class QuizSetupComponent implements OnInit {
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
-  // Specifies if category and difficulty shall be visible on html 
+  // Specifies if category and difficulty shall be visible on html
   showMode: boolean = true;
   showCategory: boolean = false;
   showDifficulty: boolean = false;
@@ -22,15 +22,25 @@ export class QuizSetupComponent implements OnInit {
 
   showHardButton: boolean = true;
 
+  breadPlayer: string;
+  breadCategory: string;
+
 
   setMode(mode){
     this.choosenMode = mode;
     this.showMode = false;
     this.showCategory = true;
 
+    if(this.choosenMode == '1'){
+      this.breadPlayer = 'One player';
+    }
+    else{
+      this.breadPlayer = 'Multiplayer';
+    }
+
   }
 
-  // Removes categories and adds difficultylevels on html 
+  // Removes categories and adds difficultylevels on html
   setCategory(category){
     this.choosenCategory = category;
     this.showCategory = false;
@@ -38,6 +48,26 @@ export class QuizSetupComponent implements OnInit {
     if(category == '21' || category == '27' || category == '18' || category == '32'){
       this.showHardButton = false;
     }
+
+    if(this.choosenCategory == '9'){
+      this.breadCategory = 'General';
+    }
+    else if(this.choosenCategory == '21'){
+      this.breadCategory = 'Sports';
+    }
+    else if(this.choosenCategory == '23'){
+      this.breadCategory = 'History';
+    }
+    else if(this.choosenCategory == '27'){
+      this.breadCategory = 'Animals';
+    }
+    else if(this.choosenCategory == '18'){
+      this.breadCategory = 'Sci- Computers';
+    }
+    else if(this.choosenCategory == '32'){
+      this.breadCategory = 'Cartoons';
+    }
+
   }
 
   setDifficulty(difficulty){
